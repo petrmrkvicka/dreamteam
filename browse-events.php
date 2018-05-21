c3b941e0815863cc66527c74a9a0fc122418ce7b
szangana <33278006+szangana@users.noreply.github.com>
2018-05-21 09:04:48 +0200

    Browse Page Created
    Empty Browse HTML Page

---
+++ browse-events.php
@@ -0,0 +1,25 @@
+<?php
+  //variables set for head.html, where these are used to show difference between pages
+  $title = "Browse Event Search Results at Dokk1";
+  $descr = "";
+  $keywords = "";
+  //includes everything from file below, without the file, website will show an error
+  require("inc/head.html");
+  // covered until </head>
+ ?>
+
+    <body>
+        <div class="wrapper">
+            <?php require("inc/header.html"); ?>
+            <!-- take a look at the header.html file, there is a comment for you:) -->
+            <main class="content">
+
+
+            </main>
+            <?php require("inc/footer.html"); ?>
+        </div>
+        <!-- javaScript -->
+      
+    </body>
+
+    </html>
