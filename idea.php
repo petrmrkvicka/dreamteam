5805c7de141a961ada484b6bc95daa3875e7ec4b
Petr <petr@petrmrkvicka.com>
2018-05-23 07:41:03 +0200

    merging

--- idea.php
+++ idea.php
@@ -1,6 +1,6 @@
 <?php
   //variables set for head.html, where these are used to show difference between pages
-  $title = "Home of events at Dokk1";
+  $title = "Submit your idea! - Dokk1";
   $descr = "";
   $keywords = "";
   //includes everything from file below, without the file, website will show an error
@@ -9,27 +9,27 @@
  ?>
 
     <body>
-
+      <div class="wrapper">
         <?php require("inc/header.html"); ?>
         <!-- take a look at the header.html file, there is a comment for you:) -->
         <main class="content">
 
             <section class="form--idea">
-                
-                
+
+
                      <a class="button--back" href="index.php">back</a>
-                
+
                 <h2 class="form--idea-h2">Do you have an awesome idea for an event?</h2>
                 <p class="form--idea-p">Submit it here and we will help you to turn it into reality!</p>
 
                 <form>
 
                     <textarea placeholder="Tell us your idea!" class="type--idea"></textarea>
-                    
+
                     <input type="text" name="your name" placeholder="Your Name">
-                    
+
                     <input type="text" name="your email" placeholder="Your email">
-                    
+
                     <input class="button--form" type="submit" value="SEND">
                 </form>
 
